5eefdd0e6784188a37d256def58dcb0e556d5114
abdalrhman <radea4082@gmail.com>
2025-07-23 03:09:44 +0300

    update1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
           I'm a Full Stack developer with 1 years of experience using React and
           NodeJS . Reach out if you'd like to learn more!
         </p>
-        <a href="https://drive.google.com/file/d/1e-IO7SupcsfLeLwNjT0pxWaVmbfkfG_3/view?usp=sharing"  download className={styles.contactBtn}>
+        <a href="https://drive.google.com/file/d/1tnGdii8mqQ0j05E-TDTgxhwtoaZvAn9D/view?usp=sharing"  download className={styles.contactBtn}>
          Download CV
         </a>
       
